5cd869fc6aa6ddf1b850df56aa186996227d530d
Harsh Bharvada <harsh07bharvada@gmail.com>
2020-05-16 16:35:12 +0530

    Card added

--- js/home.js
+++ js/home.js
@@ -1,17 +1,40 @@
-window.onload = loadingData();
+window.onload = (event) => {
+  console.log('page is fully loaded');
+  loadingData();
+};
 
 
 const loadingData = ()=>{
 
-    const result = getData('https://sidie.herokuapp.com/getProjects');
-    console.log(`Result : ${result}`);
+    getData('https://sidie.herokuapp.com/getProjects')
+    .then(data=>{
+      if(data.status == 401)
+      {
+        window.location = '/';
+      }
+      else
+      {
+        console.log(data);
+        populatePage(data);
+      }
+    });
+    
 
+}
+const populatePage = (data)=>{
+  const {username,result} = data;
+  const usernameField = document.getElementById('username');
+  usernameField.innerHTML = username;
+  result.forEach(project => {
+    addCard(project);
+  });
 }
 
 const signout = ()=>{
 
     const result = getData('https://sidie.herokuapp.com/signout');
     console.log(`Result : ${result}`);
+    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location = '/'
     
 }
@@ -20,14 +43,64 @@ async function getData(url = '') {
     
     const response = await fetch(url, {
       method: 'GET', 
-      mode: 'cors', 
-      cache: 'no-cache', 
-      credentials: 'same-origin', 
+      mode:'cors',
       headers: {
-        'Content-Type': 'application/json'
-      },
-      redirect: 'follow', 
-      referrerPolicy: 'no-referrer'
+        'Accept': 'application/json',
+        'Authorization':"Bearer "+document.cookie.split("=")[1]
+      }
     });
     return response.json(); 
+}
+
+
+const addCard = ({_id,name,description,status,link})=>{
+
+    const mainSection = document.getElementById('cardSection');
+    
+    const htmlToBeAdded = `<!-- Card -->
+    <div class="flex flex-col bg-white w-full md:max-w-md h-auto rounded-lg justify-around items-center p-8">
+
+        <!-- Project Name -->
+        <div class="flex  w-full h-auto  md:py-5 rounded-md justify-center items-center">
+            <span id="${_id}-name" class="text-gray-900 font-bold md:text-2xl "> ${name} </span>
+        </div>
+
+        <!-- Project Info Status & Link -->
+        <div class="flex flex-col bg-card-info w-full h-auto rounded-lg p-8 justify-center items-center">
+            <div class="flex w-full h-auto text-card-info text-center md:text-md font-semibold justify-center items-center">${description}</div>
+        
+            <div class="flex w-full h-auto justify-between items-center">
+                <div id="${_id}-link" class=" flex w-9/12 h-12 mt-8 bg-link-box text-center rounded-md justify-center items-center">
+                    ${link}
+                </div>
+
+                <div class=" flex w-2/12 h-12 mt-8 bg-link-box text-center rounded-md justify-center items-center">
+                    <svg class="cursor-pointer" onclick="window.open('${link}')" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
+                </div>
+                
+            </div>
+
+            <div class=" flex status-${status} w-full h-12 mt-4 bg-link-box text-center rounded-md justify-center items-center">
+                ${status}
+            </div>
+        </div>
+
+        <div class="flex w-full h-auto mt-4 justify-between items-center">
+
+            <div id="${_id}-edit"  class="flex w-5/12 h-12 edit-button rounded-md justify-center items-center">
+                
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#b7791f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+                <span class="text-xl font-semibold px-2">Edit</span>
+            </div>
+
+            <div id="${_id}-delete" class="flex w-5/12 h-12 delete-button rounded-md justify-center items-center">
+                
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#e53e3e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+                <span class="text-xl font-semibold px-2">Delete</span>
+            </div>
+
+        </div>
+    </div>`;
+    mainSection.innerHTML +=htmlToBeAdded;
+
 }
--- js/main.js
+++ js/main.js
@@ -1,17 +1,58 @@
 const signin=()=>{
     const username = document.getElementById('username');
     const password = document.getElementById('password');
-    let formData = new FormData();
-    formData.append("username", username.value);
-    formData.append("password", password.value);
-    const options = {
-        method: "POST",
-        body: formData,
-      };
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("username", "sunny");
+    urlencoded.append("password", "pass");
 
-    fetch("https://sidie.herokuapp.com/signin", options)
-      .then((res) => res.json())
-      .then(data=>console.log(data))
-      .catch(err=>console.log(err));
+    postData("https://sidie.herokuapp.com/signin",urlencoded)
+    .then(data=>{
+      console.log(data);
+      if(data.token)
+      {
+        document.cookie = "token="+data.token;
+        window.location = '../home.html';
+      }
+    })
+    .catch(err=>{
+      console.log(err);
+    });
+
+    
+
+}
+const signup=()=>{
+  const username = document.getElementById('username');
+  const password = document.getElementById('password');
+  const formData = new FormData();
+  formData.append("username", username.value);
+  formData.append("password", password.value);
+  const options = {
+      method: "POST",
+      body: formData,
+    };
+
+  fetch("https://sidie.herokuapp.com/signup", options)
+    .then((res) => res.json())
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err));
 }
 
+
+
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors', 
+    cache: 'no-cache', 
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', 
+    referrerPolicy: 'no-referrer', 
+    body: data 
+  });
+  return response.json(); 
+}
