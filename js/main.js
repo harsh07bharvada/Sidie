89b7dbbb7cba7bce2c97aee205063aba3b5f4598
Harsh Bharvada <harsh07bharvada@gmail.com>
2020-05-19 21:13:44 +0530

    added dialogs after signing in and signing up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+//Dialog Polyfill
+dialogPolyfill.registerDialog(document.getElementById("sign-status-modal"));
+//End
+
 //Sign In Function
 const signin=()=>{
     const usernameValue = document.getElementById('username').value;
@@ -109,25 +113,50 @@ const hideError = (wrapperID)=>{
 //Fetch helper function 
 const callFetch=(process,urlencoded)=>{
 
+  document.getElementById("sign-status-modal").showModal();
   postData("https://sidie.herokuapp.com/"+process,urlencoded)
     .then(data=>{
       console.log(data);
       if(data.token)
       {
+        document.getElementById("loading-icon").classList.add("p-2","rounded-full","border-2","border-green-500");
+        document.getElementById("loading-icon").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#48bb78" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>`
+        document.getElementById("loading-text").innerHTML = `Successfully signed in!`;
         document.cookie = "token="+data.token;
-        window.location = '../home.html';
+        setTimeout(()=>{
+          window.location = '../home.html';
+        },2000);
+        
       }
       else if(data.error === "Authentication Error")
       {
+        document.getElementById("loading-icon").classList.add("p-2","rounded-full","border-2","border-red-500");
+        document.getElementById("loading-icon").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#f56565" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg>`
+        document.getElementById("loading-text").innerHTML = `Issue with the details please try again!`;
+        setTimeout(()=>{
+          document.getElementById("signup-status-modal").showModal();
+        },2000);
         showError("Incorrect Username / Password","error-wrapper","error-text");
       }
       else if(data.status === 409 || data.status === 500)
       {
+        document.getElementById("loading-icon").classList.add("p-2","rounded-full","border-2","border-red-500");
+        document.getElementById("loading-icon").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#f56565" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg>`
+        document.getElementById("loading-text").innerHTML = `Issue with the details please try again!`;
+        setTimeout(()=>{
+          document.getElementById("signup-status-modal").showModal();
+        },2000);
         showError("Username already exists","error-wrapper","error-text");
       }
       else if(data.status === 201)
       {
-        window.location = '../signin.html';
+        document.getElementById("loading-icon").classList.add("p-2","rounded-full","border-2","border-green-500");
+        document.getElementById("loading-icon").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#48bb78" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>`
+        document.getElementById("loading-text").innerHTML = `Successfully created your account!`;
+        setTimeout(()=>{
+          window.location = '../signin.html';
+        },3500);
+        
       }
     })
     .catch(err=>{
